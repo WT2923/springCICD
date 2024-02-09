9dced9cd6e22b7ccd01acebdcbe12dd815a3013a
PF48Y76P\User <sahil.gevariya@wissen.com>
2024-02-09 15:11:27 +0530

    Added github webhook

--- src/test/java/com/springboottest/SpringBootTest/UserDataControllerTest.java
+++ src/test/java/com/springboottest/SpringBootTest/UserDataControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +17,9 @@ public class UserDataControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     public void testRegistrationAndLogin() {
         // Registration
@@ -23,8 +27,10 @@ public class UserDataControllerTest {
         ResponseEntity<UserData> registrationResponse = restTemplate.postForEntity("/register", registrationRequest, UserData.class);
 
         assertEquals(200, registrationResponse.getStatusCodeValue());
+
         UserData registeredUserData = registrationResponse.getBody();
-        assertEquals("testuser", registeredUserData.getUsername());         
+        assertEquals("testuser", registeredUserData.getUsername());
+        assertEquals(passwordEncoder.encode("password123"), registeredUserData.getPassword());
     }
 
     @Test
@@ -32,10 +38,12 @@ public class UserDataControllerTest {
     public void testLogin() {
         // Login
         UserAccessRequest loginRequest = new UserAccessRequest("testuser", "password123");
-        ResponseEntity<UserData> loginResponse = restTemplate.postForEntity("/api/users/login", loginRequest, UserData.class);
+        ResponseEntity<UserData> loginResponse = restTemplate.postForEntity("/login", loginRequest, UserData.class);
 
         assertEquals(200, loginResponse.getStatusCodeValue());
+
         UserData loggedInUser = loginResponse.getBody();
         assertEquals("testuser", loggedInUser.getUsername());
+        assertEquals(passwordEncoder.encode("password123"), loggedInUser.getPassword());
     }
 }
